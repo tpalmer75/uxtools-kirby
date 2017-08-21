39ed9c7c03b1c69b699431f7e187e8e3a9ea1cbe
lucid-taylor <taylorpalmer@Taylors-Lucid-Macbook-Pro.local>
2017-08-21 08:40:11 -0600

    hero header

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,12 +1,18 @@
 <?php snippet('header') ?>
-
+<section class="hero">
+  <div class="wrapper">
+    <h1>👋 Hi, I’m <a href="/about">Taylor</a>. I compare new UX tools so you don’t have to.</h1>
+    <a href="/tools"><button class="blue">See the tools <?= (new Asset("assets/images/icons/arrow-right.svg"))->content() ?></button></a>
+  </div>
+</section>
 <section>
       <div class="wrapper">
         <div class="columns reverse">
           <div class="content">
             <h2>The ultimate comparison of UX industry tools.</h2>
             <p>There's a new design tool every week. What's the difference? See how these design, prototyping, collaboration, and monitoring tools work for you.</p><a href="/tools">
-              <button class="blue">See the Tools </button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"><path fill="#fff" fill-rule="evenodd" d="M0 6.92v2h12.01l-5.5 5.5 1.4 1.42 7.93-7.92L7.92 0 6.5 1.41l5.5 5.5z"/></svg></a>
+              <button class="blue">See the Tools <?= (new Asset("assets/images/icons/arrow-right.svg"))->content() ?>
+              </button></a>
           </div>
           <div class="content"><a href="/tools">
               <div class="landing-image"><img src="assets/images/landing-image-logos.png" alt="App Logos"/><img src="assets/images/bottom-shadow.png" class="shadow-image"/></div></a></div>
@@ -26,7 +32,7 @@
           <div class="content">
             <h2>The ultimate list of UX industry books.</h2>
             <p>What's everyone reading these days? It’s hard to know where to start in a big industry like UX design. See the most popular and recommended books in the industry.</p><a href="/library">
-              <button class="blue">Browse the Library</button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"><path fill="#fff" fill-rule="evenodd" d="M0 6.92v2h12.01l-5.5 5.5 1.4 1.42 7.93-7.92L7.92 0 6.5 1.41l5.5 5.5z"/></svg></a>
+              <button class="blue">Browse the Library <?= (new Asset("assets/images/icons/arrow-right.svg"))->content() ?></button></a>
           </div>
         </div>
       </div>
